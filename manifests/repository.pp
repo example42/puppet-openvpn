f6b022b7eae6eb573e2b467ca3f915a47cf52718
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2013-10-11 17:53:18 +0200

    Adding appropriate fw rules when configuring openvpn repo

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -10,15 +10,36 @@ class openvpn::repository (
       default: { $distro = $::lsbdistcodename }
     }
 
+    if $openvpn::bool_firewall {
+      firewall { 'openvpn-repository':
+        destination    => '173.192.224.173', # repos.openvpn.net
+        protocol       => 'tcp',
+        port           => 80,
+        direction      => 'output', 
+        enable_v6      => false
+      }
+    }
+
     apt::repository { 'openvpn':
       url        => "http://repos.openvpn.net/repos/apt/${distro}-snapshots",
       distro     => $distro,
       repository => 'main',
     }
 
+    if $openvpn::bool_firewall {
+      firewall { 'openvpn-repository-key':
+        destination    => '173.192.224.173', # repos.openvpn.net
+        protocol       => 'tcp',
+        port           => 443,
+        direction      => 'output',
+        enable_v6      => false,
+      }
+    }
+
     apt::key { '2048R/E158C569':
       url => 'https://swupdate.openvpn.net/repos/repo-public.gpg',
     }
+
   }
 
 }
